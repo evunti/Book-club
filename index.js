3a7b682b63f738aa1e55cb77401dc163389027a2
Jen <evunti@gmail.com>
2024-12-11 09:39:04 -0500

    creating bookclub version 2.0

---
+++ index.js
@@ -0,0 +1,20 @@
+const bookOne = {
+  name: "I'm Glad My Mom Died",
+  author: "Jennette Mcurdy",
+};
+const bookTwo = {
+  name: "Educated",
+  author: "Tara Westover",
+};
+const bookThree = {
+  name: "All The Lovers In The Night",
+  author: "Meiko Kawakami",
+};
+const bookFour = {
+  name: "The Hitchhikers Guid To The Galaxy",
+  author: "Douglas Adams",
+};
+const bookFive = {
+  name: "Persuasion",
+  author: "Jane Austen",
+};
